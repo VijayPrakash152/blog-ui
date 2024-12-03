4ad1eddabd2bd2d84e6d25997034046b95104911
vijayprakash <vijay.prakash@mediassist.in>
2024-12-04 00:54:58 +0530

    added types for gtag

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,7 +4,7 @@ interface Window {
     ReactNativeWebView?: {
       postMessage: (message: string) => void;
     };
-    gtag: (...args: any[]) => void;
+    gtag: (command: GtagCommand, idOrEventName: string, params?: GtagParams) => void;
     webkit?: {
       messageHandlers?: {
         nativeHandler?: {
@@ -17,4 +17,21 @@ interface Window {
     interface IntrinsicElements {
         'hyvor-talk-comments': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
     }
+}
+
+type GtagCommand = 'config' | 'event';
+
+interface GtagParams {
+  // Common parameters for both 'config' and 'event'
+  [key: string]: string | number | boolean | undefined;
+
+  // 'config' specific parameters
+  page_title?: string;
+  page_location?: string;
+  page_path?: string;
+
+  // 'event' specific parameters
+  event_category?: string;
+  event_label?: string;
+  value?: number;
 }
